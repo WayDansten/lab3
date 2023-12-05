c6649e05f20cde7f852e6be9ddbcebc80767ba50
WayDansten <145903205+WayDansten@users.noreply.github.com>
2023-12-05 09:35:14 +0300

    Update Roof.java

--- objects/Roof.java
+++ objects/Roof.java
@@ -5,16 +5,27 @@ import enums.RoofMaterial;
 public class Roof {
     private RoofMaterial roofMaterial;
 
-    public Roof(RoofMaterial roofMaterial) {this.roofMaterial = roofMaterial;}
+    public Roof(RoofMaterial roofMaterial) {
+        this.roofMaterial = roofMaterial;
+    }
 
-    public void setRoofMaterial(RoofMaterial roofMaterial) {this.roofMaterial = roofMaterial;}
-    public RoofMaterial getRoofMaterial() {return this.roofMaterial;}
+    public void setRoofMaterial(RoofMaterial roofMaterial) {
+        this.roofMaterial = roofMaterial;
+    }
+    
+    public RoofMaterial getRoofMaterial() {
+        return this.roofMaterial;
+    }
 
     @Override
-    public String toString() {return "Крыша из " + this.roofMaterial.toString();}
+    public String toString() {
+        return "Крыша из " + this.roofMaterial.toString();
+    }
 
     @Override
-    public int hashCode() {return this.roofMaterial.hashCode();}
+    public int hashCode() {
+        return this.roofMaterial.hashCode();
+    }
 
     @Override
     public boolean equals(Object obj) {
