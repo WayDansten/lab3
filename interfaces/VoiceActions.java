1af5cc0a450cf456449782bb3d81f82e337fc0c8
WayDansten <145903205+WayDansten@users.noreply.github.com>
2023-12-05 09:30:21 +0300

    Update VoiceActions.java

--- interfaces/VoiceActions.java
+++ interfaces/VoiceActions.java
@@ -1,10 +1,5 @@
 package interfaces;
 
-import enums.Gender;
-import enums.Truth;
-import objects.Human;
-
 public interface VoiceActions {
-    public void say(String message, Human h, Truth t);
-    public void say(String message, Truth t);
+    public void say(String phrase);
 }
